486b500a83f4d775979df957c6b9e7c7bb6b94db
Matvey2009 <matvey2009ua@gmail.com>
2021-01-24 18:41:36 +0300

    Update Form.py

--- modules/Form.py
+++ modules/Form.py
@@ -1,19 +1,8 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'formcfNsRU.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-
-class Ui_AuxProg(object):
+class Ui_AuxProg(QMainWindow):
     def setupUi(self, AuxProg):
         if not AuxProg.objectName():
             AuxProg.setObjectName(u"AuxProg")
