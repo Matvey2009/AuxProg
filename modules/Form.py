881991380fd3e6dfa6943ee4764cf524fb1bb223
Matvey2009 <matvey2009ua@gmail.com>
2021-02-13 21:05:00 +0300

    Доделки

--- modules/Form.py
+++ modules/Form.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'backup0opUYJG.ui'
+## Form generated from reading UI file 'backup0uNwxAy.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,68 +17,17 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(220, 156)
+        Form.resize(111, 61)
         icon = QIcon()
         icon.addFile(u"D:/Documents/Pictures/\u0437\u0430\u0433\u0440\u0443\u0436\u0435\u043d\u043e.jpg", QSize(), QIcon.Normal, QIcon.Off)
         Form.setWindowIcon(icon)
         Form.setStyleSheet(u"")
         self.pushButton = QPushButton(Form)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(50, 50, 111, 61))
+        self.pushButton.setGeometry(QRect(0, 0, 111, 61))
         self.pushButton.setStyleSheet(u"QWidget {\n"
 "	background-color: rgb(0, 0, 0);\n"
 "	border: 4px solid rgb(255, 0, 0);\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Pos_x = QLabel(Form)
-        self.Pos_x.setObjectName(u"Pos_x")
-        self.Pos_x.setGeometry(QRect(30, 130, 51, 21))
-        self.Pos_x.setStyleSheet(u"QWidget {\n"
-"	background-color: rgb(0, 0, 0);\n"
-"	border: 2px solid rgb(255, 0, 0);\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Pos_y = QLabel(Form)
-        self.Pos_y.setObjectName(u"Pos_y")
-        self.Pos_y.setGeometry(QRect(130, 130, 51, 21))
-        self.Pos_y.setStyleSheet(u"QWidget {\n"
-"	background-color: rgb(0, 0, 0);\n"
-"	border: 2px solid rgb(255, 0, 0);\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Xp = QLabel(Form)
-        self.Xp.setObjectName(u"Xp")
-        self.Xp.setGeometry(QRect(40, 110, 48, 13))
-        self.Xp.setStyleSheet(u"QWidget\n"
-"{\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Yp = QLabel(Form)
-        self.Yp.setObjectName(u"Yp")
-        self.Yp.setGeometry(QRect(130, 110, 48, 13))
-        self.Yp.setStyleSheet(u"QWidget\n"
-"{\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Freething = QCheckBox(Form)
-        self.Freething.setObjectName(u"Freething")
-        self.Freething.setGeometry(QRect(130, 20, 91, 17))
-        self.Freething.setStyleSheet(u"QWidget\n"
-"{\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.KOL_CLICK_SEK = QLineEdit(Form)
-        self.KOL_CLICK_SEK.setObjectName(u"KOL_CLICK_SEK")
-        self.KOL_CLICK_SEK.setGeometry(QRect(10, 20, 91, 20))
-        self.KOL_CLICK_SEK.setStyleSheet(u"QWidget\n"
-"{\n"
-"	color: rgb(255, 0, 0);\n"
-"}")
-        self.Kol_click_sek = QLabel(Form)
-        self.Kol_click_sek.setObjectName(u"Kol_click_sek")
-        self.Kol_click_sek.setGeometry(QRect(0, 0, 111, 16))
-        self.Kol_click_sek.setStyleSheet(u"QWidget\n"
-"{\n"
 "	color: rgb(255, 0, 0);\n"
 "}")
         self.Fon = QGraphicsView(Form)
@@ -89,14 +38,7 @@ class Ui_Form(object):
 "}")
         self.Fon.raise_()
         self.pushButton.raise_()
-        self.Pos_x.raise_()
-        self.Pos_y.raise_()
-        self.Xp.raise_()
-        self.Yp.raise_()
-        self.Freething.raise_()
-        self.KOL_CLICK_SEK.raise_()
-        self.Kol_click_sek.raise_()
-        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+
         self.retranslateUi(Form)
 
         QMetaObject.connectSlotsByName(Form)
@@ -105,11 +47,5 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"AuxProg", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"\u041d\u0430\u0436\u0438\u043c\u0430\u0439 \u0441\u044e\u0434\u0430", None))
-        self.Pos_x.setText("")
-        self.Pos_y.setText("")
-        self.Xp.setText(QCoreApplication.translate("Form", u"      X", None))
-        self.Yp.setText(QCoreApplication.translate("Form", u"      Y", None))
-        self.Freething.setText(QCoreApplication.translate("Form", u"\u0417\u0430\u043c\u0430\u043e\u0440\u043e\u0437\u043a\u0430", None))
-        self.Kol_click_sek.setText(QCoreApplication.translate("Form", u"\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u043a\u043b\u0438\u043a\u043e\u0432", None))
     # retranslateUi
 
--- modules/Main.py
+++ modules/Main.py
@@ -1,4 +1,6 @@
-
+import mouse
+import keyboard
+from time import  sleep
 from PySide2 import QtCore, QtWidgets
 from modules.Form import Ui_Form
 import pyautogui as a
@@ -19,6 +21,7 @@ class Main(Ui_Form, QtWidgets.QWidget):
         self.timer.timeout.connect(self.autoclick)
         self.click_status = False
         self.pushButton.clicked.connect(self.Button_Click)
+        keyboard.hook(self.print_present_key)
 
     def Button_Click(self):
         """ Переключатель Автоклика """
@@ -33,15 +36,23 @@ class Main(Ui_Form, QtWidgets.QWidget):
 
     def autoclick(self):
         """ Автоклик """
-        a.tripleClick()
+        # a.tripleClick()
+        for i in range(10):
+            mouse.click()
+        sleep(0.01)
+
     def mouseMoveEvent(self, e):
         """ Позицыя курсора """
         self.label_6.setText(str(e.x()))
         self.label_7.setText(str(e.y()))
 
-    def keyPressEvent(self, e):
-        """ Горячие клавиши """
-        if e.key():
-            self.label_5.setText(str(chr(e.key())))
-        e.assept()
-
+    # def keyPressEvent(self, e):
+       # """ Горячие клавиши(qt) """
+       # if e.key():
+        # self.label_5.setText(str(chr(e.key())))
+       # e.assept()
+    def print_present_key(self, e):
+        """ Горячие клавиши(keyboard) """
+        print(e, e.event_type, e.name)
+        if e.event_type == "up":
+            self.Button_Click()
